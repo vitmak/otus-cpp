17db6c76d42dba86e13c32d44078698be395364f
vitmak <vitaly.makarevich@gmail.com>
2021-08-22 07:25:43 +0700

    Instantiating standart std::map <int, int>. Filling with 10 elements.

--- source/main.cpp
+++ source/main.cpp
@@ -1,6 +1,18 @@
 #include "reserving_allocator.h"
+#include<iostream>
+#include<map>
 
 int main() {
+    // Fill standart map
+    auto standartMap = std::map<int, int>{};
+
+    standartMap[0] = 1;
+    for (auto i = 1; i < 10; ++i)
+        standartMap[i] = i * standartMap[i - 1];
+
+    for (const auto& v : standartMap) {
+        std::cout << v.first << " " << v.second << std::endl;
+    }
 
     return 0;
 }
