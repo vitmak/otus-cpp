e3b1c7353b64bb35780785e55cb8d02a74c8f9b4
vitmak <vitaly.makarevich@gmail.com>
2021-09-15 17:31:21 +0700

    Added print_ip function for std::string type. Modified returns type for integral function.

--- main.cpp
+++ main.cpp
@@ -2,9 +2,13 @@
 #include <type_traits>
 #include <bitset>
 #include <vector>
+#include <string>
 
-template <typename T, typename Fake = std::enable_if_t<std::is_integral<T>::value>>
-void print_ip(T&& value) {
+//
+// print_ip  - function for integral types
+//
+template <typename T> 
+typename std::enable_if_t<std::is_integral_v<T>> print_ip(const T& value) {
 	const auto ByteSize = 8;
 	
 	std::bitset<sizeof(T) * ByteSize> bytes = value;
@@ -29,17 +33,37 @@ void print_ip(T&& value) {
 	std::cout << std::endl;
 }
 
+//
+// print_ip  - function for string type
+//
+template <typename S>
+typename std::enable_if_t<std::is_same_v<S, std::string>> print_ip(const S& str) {
+	std::cout << str << std::endl;
+}
+
+//template <typename T>
+//void print_ip(T&& container) {
+//	std::cout << __PRETTY_FUNCTION__ << std::endl;
+//	for (auto it = container.cbegin(); it != container.cend(); ++it) {
+//		if (it != container.cbegin())
+//			std::cout << ".";
+//		std::cout << *it;
+//	}
+//	std::cout << std::endl;
+//}
+
 int main() {
 	print_ip(char{-1});
 	print_ip(short{ 0 });
-
 	print_ip(int{ 2130706433 });
 	print_ip(long long{ 8875824491850138409 });
-	//print_ip(std::string{"std::string"});
 	
-	/*std::vector<int> vec{255, 0, 0, 0};
-	print_ip(vec);
+	print_ip(std::string{"trololo"});
+	
+	//std::vector<int> vec{0, 1, 2, 3};
+	//print_ip(vec);
 
+	/*
 	std::list<short> arr;
 	arr.push_back(short{1});
 	arr.push_back(short{ 2 });*/
