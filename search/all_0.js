5f8b5523829864c6f545c5959e6085b4b655b2cc
vitmak <vitmak@users.noreply.github.com>
2021-09-20 16:18:06 +0000

    deploy: d7e522167db93094ed2c638599ad690bbdd72d1a

---
+++ search/all_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['otus_2dcpp_0',['otus-cpp',['../md_README.html',1,'']]]
+];
---
+++ search/pages_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['otus_2dcpp_1',['otus-cpp',['../md_README.html',1,'']]]
+];
