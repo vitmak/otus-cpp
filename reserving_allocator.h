91d0012887091634432a0ca4035d492fbd4695b7
vitmak <vitaly.makarevich@gmail.com>
2021-09-22 15:21:38 +0700

    Usage allocator_traits

--- reserving_allocator.h
+++ reserving_allocator.h
@@ -2,15 +2,11 @@
 
 #include"memory_manager.h"
 #include <cstdio>
+#include <memory>
 
 template<typename T, size_t S>
 struct reserving_allocator {
     using value_type = T;
-
-    using pointer = T*;
-    using const_pointer = const T*;
-    using reference = T&;
-    using const_reference = const T&;
 
     template<typename U>
     struct rebind {
@@ -27,7 +23,7 @@ struct reserving_allocator {
 
     }
 
-    pointer allocate(std::size_t n) {
+    value_type* allocate(std::size_t n) {
         if (n > S)
             throw std::bad_alloc();
 
@@ -37,10 +33,10 @@ struct reserving_allocator {
         return addr;
     }
 
-    void deallocate(pointer p, std::size_t n) {
+    void deallocate(value_type* p, std::size_t n) {
         m_memoryMng.SetMemoryState(p, n, MemoryStates::eFree);
     }
-
+    
     template<typename U, typename ...Args>
     void construct(U* p, Args &&...args) {
         new(p) U(std::forward<Args>(args)...);
@@ -48,6 +44,6 @@ struct reserving_allocator {
 
     template<typename U>
     void destroy(U* p) {
-        ((pointer)p)->~T();
+        ((value_type*)p)->~T();
     }
 };
