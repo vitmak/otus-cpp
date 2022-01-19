399bf40183dc50466134a738bf8eecca086c1ddd
vitmak <vitaly.makarevich@gmail.com>
2022-01-19 14:11:46 +0700

    Added Boost library.

--- main.cpp
+++ main.cpp
@@ -1,42 +1,35 @@
-#include <iostream>
-
 #include "async.h"
-
-#include <thread>
-#include <chrono>
-
-namespace async {
-
-using handle_t = void *;
-
-handle_t connect(std::size_t bulk);
-void receive(handle_t handle, const char *data, std::size_t size);
-void disconnect(handle_t handle);
-
-}
+#include <iostream>
+#include <boost/asio.hpp>
 
 
-int main(int, char *[]) {
-    std::size_t bulk = 5;
-    auto h = async::connect(bulk);
-    auto h2 = async::connect(bulk);
-        
-    async::receive(h, "1", 1);
+using boost::asio::ip::tcp;
 
-    async::receive(h2, "cmd1\n", 5);
-    async::receive(h2, "cmd2\n", 5);
-    
-    async::receive(h, "\n2\n3\n4\n5\n6\n{\na\n", 15);
-    async::receive(h, "b\nc\nd\n}\n89\n", 11);
+int main(int argc, char* argv[])
+{
+    try
+    {
+        if (argc < 2)
+        {
+            std::cerr << "Usage: chat_server <port> [<port> ...]\n";
+            return 1;
+        }
 
-    async::receive(h2, "cmd3\n", 5);
-    async::receive(h2, "cmd4\n", 5);
-    async::receive(h2, "cmd5\n", 5);
+        boost::asio::io_service io_service;
 
-    using namespace std::chrono_literals;
-    std::this_thread::sleep_for(3000ms);
-    async::disconnect(h);
-    async::disconnect(h2);
+        /*std::list<chat_server> servers;
+        for (int i = 1; i < argc; ++i)
+        {
+            tcp::endpoint endpoint(tcp::v4(), std::atoi(argv[i]));
+            servers.emplace_back(io_service, endpoint);
+        }
+
+        io_service.run();*/
+    }
+    catch (std::exception& e)
+    {
+        std::cerr << "Exception: " << e.what() << "\n";
+    }
 
     return 0;
 }
